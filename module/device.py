16648451d8932c400d66d40bb37a497f134ee647
Akhilesh Muthusamy <akhileshm@outlook.in>
2021-10-22 22:50:51 -0400

    Added GET request filter records by device id

---
+++ module/device.py
@@ -0,0 +1,26 @@
+from flask import jsonify
+from flask_restful import Resource, reqparse
+
+from app import mongo
+
+
+parser = reqparse.RequestParser()
+
+
+
+class DeviceInfo(Resource):
+
+
+    def get(self):
+        parser.add_argument('id', type=int)
+        args = parser.parse_args()
+
+        try:
+            if args['id']:
+                records = mongo.db.sensor_readings.find({'engine_no': args['id']})
+                output = [record for record in records]
+                return jsonify({'message': 'Records fetched successfully!', 'error': False, 'data': output})
+
+        except Exception as e:
+            print('\033[31m' + 'Exception in get function of DeviceInfo class', str(e), sep='\n', end='\033[0m\n')
+            return jsonify({'message': 'Error while fetching records', 'error': True, 'data': None})
